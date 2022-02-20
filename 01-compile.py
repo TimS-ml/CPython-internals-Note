8eccc11f4aa64d00c04c27f51420b1891439f96b
TimS <jianshen@pku.edu.cn>
2022-02-20 10:30:42 -0800

    fix byte code

--- 01-compile.py
+++ 01-compile.py
@@ -1,11 +1,13 @@
 # Let's dig inside the function "compile"
 # help(compile)
 
-c = compile('test.py', 'test.py', 'exec')
+# c = compile('Your code in text format', 'test.py', 'exec')
+c = compile(open('test.py').read(), 'test.py', 'exec')
 print(c)
 
 # the most important one is the 'co_code'
 print(dir(c))
 
 print(c.co_code)  # a str type thing, a byte code
-print([b for b in c.co_code])  # check ASC table
+print([b for b in c.co_code])  # check ASC2 table
+print(len(c.co_code))  # 28
